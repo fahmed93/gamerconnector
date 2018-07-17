370514d90774c4f765f18eb41673e5da62b42bee
Fahad Ahmed <fahmed93@gmail.com>
2018-07-17 18:39:05 -0500

    Login routes and logic, input validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://fahad:fahad1@ds139951.mlab.com:39951/gamerconnector"
+  mongoURI: 'mongodb://fahad:fahad1@ds139951.mlab.com:39951/gamerconnector',
+  secretOrKey: 'secret'
 };
