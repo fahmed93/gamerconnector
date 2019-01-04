e041e604c1ca6895450351bf1502783507340ff1
fahmed7x <fahad.ahmed@walgreens.com>
2019-01-04 11:46:41 -0600

    Create profile page

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -27,7 +27,85 @@ class CreateProfile extends Component {
     };
   }
 
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    console.log('Submit');
+  };
+
   render() {
+    const { errors, displaySocialInputs } = this.state;
+
+    let socialInputs;
+
+    if (displaySocialInputs) {
+      socialInputs = (
+        <div>
+          <InputGroup
+            placeholder="Twitter Profile URL"
+            name="twitter"
+            icon="fab fa-twitter"
+            value={this.state.twitter}
+            onChange={this.onChange}
+            error={errors.twitter}
+          />
+
+          <InputGroup
+            placeholder="Facebook Profile URL"
+            name="facebook"
+            icon="fab fa-facebook"
+            value={this.state.facebook}
+            onChange={this.onChange}
+            error={errors.facebook}
+          />
+
+          <InputGroup
+            placeholder="Twitch Channel URL"
+            name="twitch"
+            icon="fab fa-twitch"
+            value={this.state.twitch}
+            onChange={this.onChange}
+            error={errors.twitch}
+          />
+
+          <InputGroup
+            placeholder="YouTube Channel URL"
+            name="youtube"
+            icon="fab fa-youtube"
+            value={this.state.youtube}
+            onChange={this.onChange}
+            error={errors.youtube}
+          />
+
+          <InputGroup
+            placeholder="Instagram Profile URL"
+            name="instagram"
+            icon="fab fa-instagram"
+            value={this.state.instagram}
+            onChange={this.onChange}
+            error={errors.instagram}
+          />
+        </div>
+      );
+    }
+
+    // Select options for status
+    const options = [
+      { label: '* Select Status', value: 0 },
+      { label: 'Professional', value: 'Professional' },
+      { label: 'Semi-Professional', value: 'Semi-Professional' },
+      { label: 'Streamer', value: 'Streamer' },
+      { label: 'Coach', value: 'Coach' },
+      { label: 'Analyst', value: 'Analyst' },
+      { label: 'Caster', value: 'Caster' },
+      { label: 'Free Agent', value: 'Free Agent' },
+      { label: 'Other', value: 'Other' }
+    ];
+
     return (
       <div className="create-profile">
         <div className="container">
@@ -38,6 +116,92 @@ class CreateProfile extends Component {
                 Let's get some information to make your profile stand out
               </p>
               <small className="d-block pb-3">* = required fields</small>
+
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your profile URL. Your full name, team name, nickname, etc"
+                />
+
+                <SelectListGroup
+                  name="status"
+                  value={this.state.status}
+                  onChange={this.onChange}
+                  options={options}
+                  error={errors.status}
+                  info="Give us an idea of where you are at in your career"
+                />
+
+                <TextFieldGroup
+                  placeholder="Team"
+                  name="team"
+                  value={this.state.team}
+                  onChange={this.onChange}
+                  error={errors.team}
+                  info="Could be your own team or one you work/play for"
+                />
+
+                <TextFieldGroup
+                  placeholder="Website"
+                  name="website"
+                  value={this.state.website}
+                  onChange={this.onChange}
+                  error={errors.website}
+                  info="Could be your own or your team's website"
+                />
+
+                <TextFieldGroup
+                  placeholder="Location"
+                  name="location"
+                  value={this.state.location}
+                  onChange={this.onChange}
+                  error={errors.location}
+                  info="City & state suggested (eg. Boston, MA)"
+                />
+
+                <TextFieldGroup
+                  placeholder="Games"
+                  name="games"
+                  value={this.state.games}
+                  onChange={this.onChange}
+                  error={errors.games}
+                  info="Please use comma separated values (eg. Halo 3,League of Legends,Overwatch)"
+                />
+
+                <TextAreaFieldGroup
+                  placeholder="Bio"
+                  name="bio"
+                  value={this.state.bio}
+                  onChange={this.onChange}
+                  error={errors.bio}
+                  info="Tell us a little about yourself"
+                />
+
+                <div className="mb-3">
+                  <button
+                    onClick={() => {
+                      this.setState(prevState => ({
+                        displaySocialInputs: !prevState.displaySocialInputs
+                      }));
+                    }}
+                    className="btn btn-light"
+                  >
+                    Add Social Network
+                  </button>
+                  <span className="text-muted"> Optional</span>
+                </div>
+                {socialInputs}
+                <input
+                  value="Submit"
+                  className="submit"
+                  className="btn btn-info btn-block mt-4
+                "
+                />
+              </form>
             </div>
           </div>
         </div>
