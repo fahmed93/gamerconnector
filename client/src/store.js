d1679bdf1304e889234ed2bc64ede58367508a70
Fahad Ahmed <fahmed7x@walgreenco.net>
2019-02-05 13:50:47 -0600

    Incognito issue fix

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 const middleware = [thunk];
 const initialState = {};
